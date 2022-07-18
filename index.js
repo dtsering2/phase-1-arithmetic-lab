031cd0a9e8564d5cd7901c3b7f7c7f0736e14045
Tsering Tsering <dtsering2@gmail.com>
2022-07-18 18:46:52 -0400

    commit tree aebe087056090ffc0ea61f3270a1aad365903283

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2
+let num2 = 31
+let multiply = 62
 
-// Write your code here
+
+const random = Math.floor(Math.random() * 1000) + 1
+
+let num3 = 16
+let num4 = 6
+let mod = num3 % num4
+
+let max = Math.max(5,10,15,20)
